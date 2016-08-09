160a2afd34e19490ced47d46d0cab4a330bd9053
Laurent Falda <l.falda@groupe-scala.com>
2016-08-09 10:23:49 +0200

    gulp plugin'

---
+++ index.js
@@ -0,0 +1,10 @@
+﻿const gulp = require('gulp');
+const pngFallback = require('./png-fallback');
+
+gulp.task('default', () => {
+  return gulp.src('./images/*.svg')
+  .pipe(pngFallback())
+  .pipe(gulp.dest('./output/'))
+  .on('end', () => console.log('end'))
+  .on('error', error => console.error(error));
+});
